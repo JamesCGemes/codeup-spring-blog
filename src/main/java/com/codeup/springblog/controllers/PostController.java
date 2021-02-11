87ebe811b632ec2943b46f944f1a153a38b8b48a
James Gemes <james.gemes@gmail.com>
2021-02-11 16:48:16 -0600

    need to fix the view all posts

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -2,9 +2,13 @@ package com.codeup.springblog.controllers;
 
 
 import com.codeup.springblog.models.Post;
+import jdk.dynalink.linker.LinkerServices;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class PostController {
@@ -13,17 +17,25 @@ public class PostController {
 
 
     @RequestMapping(path = "posts",method = RequestMethod.GET)
-    @ResponseBody
-    public String index(){
-        return "posts index page";
-    }
+    public String postIndex(Model model){
+        Post post01 = new Post("First Post", "My first post", 1);
+        Post post02 = new Post("Second Post", "My second post", 2);
+        Post post03 = new Post("Third Post", "My third post", 3);
 
+        List<Post> postList = new ArrayList<>();
+        postList.add(post01);
+        postList.add(post02);
+        postList.add(post03);
+
+        return "posts/index";
+
+    }
 
 
 
     @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
     public String postView(Model model){
-        Post post = new Post("First Post", "This is my first paost", 1);
+        Post post = new Post("First Post", "Post number 1", 1);
         model.addAttribute("title","Single Posts");
         model.addAttribute("post", post);
         return "posts/show";
