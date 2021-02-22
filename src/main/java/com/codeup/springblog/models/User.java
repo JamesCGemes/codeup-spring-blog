aba9f0422b03cdb42749c0cef8219a3bb898360f
James Gemes <james.gemes@gmail.com>
2021-02-22 15:09:59 -0600

    Added user class

---
+++ src/main/java/com/codeup/springblog/models/User.java
@@ -0,0 +1,77 @@
+package com.codeup.springblog.models;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    List<Post> postsCreated;
+
+    public User(){
+
+    }
+
+    public User(long id, String username, String password, String email, List<Post> postsCreated) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.postsCreated = postsCreated;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Post> getPostsCreated() {
+        return postsCreated;
+    }
+
+    public void setPostsCreated(List<Post> postsCreated) {
+        this.postsCreated = postsCreated;
+    }
+}
